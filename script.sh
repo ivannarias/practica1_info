ac96a5f1787d80e1444f9691a37929aab0fc5ed2
ivan <ivan@DESKTOP-C4GLOQ3>
2024-10-07 08:58:07 +0200

    Primera càrrega 07/10

--- script.sh
+++ script.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-A=$(wc -w < supervivents.csv)
+awk -F',' '{ if (NF == 16) print $0 }' CAvideos.csv > supervivents.csv
 
-awk 'BEGIN {NS=","} $15 != "True" {print$0}' supervivents.csv > arxiuprova.csv
+cut -d',' --complement -f12,16 supervivents.csv > arxiuprova.csv
 
-B=$(wc -w < arxiuprova,csv)
+A=$(wc -l < supervivents.csv)
+
+awk '{F=","} $14 != "True"' supervivents.csv > arxiuprova.csv
+
+B=$(wc -l < arxiuprova.csv)
 
 dif=$((A-B))
 
-echo "La diferència és $dif" >> arxiuprova.csv
+echo "La diferència és" $dif
+
+awk '{F=","} {if($8 < 1000000) print"Bo"}; {if($8 >= 1000000 and $8 < 10000000) print"Excel·lent"}; {if($8 >= 10000000) print"Estrella"}'
